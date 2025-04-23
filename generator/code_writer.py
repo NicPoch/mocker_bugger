2f42c6d894bb39fdc71c13dc29931fdffabed858
Nicolas Poch Llorente <nicpoch10@gmail.com>
2025-04-23 18:52:56 -0500

    decouple code add requirements

--- generator/code_writer.py
+++ generator/code_writer.py
@@ -34,17 +34,32 @@ def render_enpoint(config, output_dir):
         includes.append(f"app.include_router({filename}.router)")
     return imports, includes
 
+def render_requirements(requirements_config,output_dir):
+    requirements_template = env.get_template("requirements_template.j2")
+    requirements_rendered = requirements_template.render(
+            requirements=requirements_config
+        )
+    filepath = f"{output_dir}/requirements.txt"
+    with open(filepath, "w") as f:
+        f.write(requirements_rendered)
+
+def render_cors(app_config):
+    cors_template = env.get_template("cors_template.j2")
+    cors_rendered = cors_template.render(
+            origins = app_config["cors"].get("origins",["*"]),
+            credentials = app_config["cors"].get("credentials",True) ,
+            methods = app_config["cors"].get("methods",["*"]) ,
+            headers = app_config["cors"].get("headers",["*"]) 
+        )
+    return cors_rendered
+
 def render_main(config, output_dir,imports,includes):
     app_config = config["app"]
     main_template = env.get_template("main_template.j2")
     main_rendered = main_template.render(
             imports=imports,
             routes=includes,
-            cors = "cors" in app_config,
-            origins = app_config["cors"].get("origins",["*"]) if "cors" in app_config else ["*"],
-            credentials = app_config["cors"].get("credentials",True) if "cors" in app_config else True,
-            methods = app_config["cors"].get("methods",["*"]) if "cors" in app_config else ["*"],
-            headers = app_config["cors"].get("headers",["*"]) if "cors" in app_config else ["*"]
+            cors = render_cors(app_config) if "cors" in app_config else None
         )
     with open(f"{output_dir}/main.py", "w") as f:
         f.write(main_rendered)
@@ -52,4 +67,5 @@ def render_main(config, output_dir,imports,includes):
 def write_application(config, output_dir="generated_api"):
     imports, includes = render_enpoint(config, output_dir)
     render_main(config, output_dir,imports,includes)
+    render_requirements(config["requirements"],output_dir)
     
